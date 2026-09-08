a8dbb317e76998ba843cb84bacf369221cb43ffb
lilsayp <lana.shevchenko.07@mail.ru>
2026-09-08 18:43:22 +0500

    Update main.go

--- golang/ch1/server2/main.go
+++ golang/ch1/server2/main.go
@@ -23,7 +23,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
-// counter echoes the number of calls so far.
 func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count %d\n", count)
